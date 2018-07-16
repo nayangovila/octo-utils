c37df0a477dfb84f23923115d85e8262748aa7e9
nayangovila <41297497+nayangovila@users.noreply.github.com>
2018-07-17 00:59:55 +0530

    made changes

--- octoUtil.js
+++ octoUtil.js
@@ -8,7 +8,30 @@
     };
     
     octoUtil.init.prototype = octoUtil.prototype = {
+        isEqualArray: function (arr1, arr2) {
+            if (arr1.length !== arr2.length) {
+                return false;
+            }
+            for (var i = 0; i < arr1.length; i++) {
+                if (arr1[i] !== arr2[i]) {
+                    return false;
+                }
+            }
+            return true;
+        },
         
+        //object has no own properties
+        isEmptyObject: function (obj) {
+            if (obj == null) {
+                return true;
+            }
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    return false;
+                }
+            }
+            return true;
+        }
     };
     
     global.octoUtil = global.OU$ = octoUtil;
