0e86084d223c85db1f6e374ff927594d5efb596d
nayangovila <41297497+nayangovila@users.noreply.github.com>
2018-07-17 00:14:27 +0530

    created base library
    created base library

---
+++ octoUtil.js
@@ -0,0 +1,15 @@
+(function (global) {
+    var octoUtil = function () {
+        return new octoUtil.init();
+    };
+    
+    octoUtil.init = function () {
+        
+    };
+    
+    octoUtil.init.prototype = octoUtil.prototype = {
+        
+    };
+    
+    global.octoUtil = global.OU$ = octoUtil;
+})(window);
